31015a1f8ee5939ab70af6dd84883195e7e4afe6
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-23 20:29:03 +0300

    adding main elements to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from axios;
 import './App.css';
 
+interface Item {
+  id: number
+  title: string
+  description: string
+}
+
 function App() {
+  const [items, setItems] = useState<Item[]>([])
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      .then((response) => {
+        const data = response.data.map((post: any) => ({
+          id: post.id,
+          title: post.title,
+          description: post.body
+        }))
+        setItems(data)
+      })
+        .catch((error) => console.error('Error fetching data:', error))
+  }, [])
+
   return (
     <div className="App">
       <h1>Card List</h1>
+      <form>
+        <input 
+          type="text"
+          value={title}
+          placeholder='Enter title...' 
+        />
+        <input 
+          type="text"
+          value={description} 
+          placeholder='Enter description...'
+        />
+        <button>Add Card</button>
+      </form>
+
+      <div className='card-list'>
+        {items.map((item) => (
+          
+        ))}
+      </div>
     </div>
   );
 }
